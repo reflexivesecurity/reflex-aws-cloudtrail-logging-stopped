b5e35d47ef5b7dff8a1e4785c9d3ecb9d7adbded
rjulian <richard@rjulian.net>
2020-04-30 13:49:48 -0700

    Fix path reference to be base path

--- terraform/sqs_lambda/reflex_aws_cloudtrail_logging_stopped.tf
+++ terraform/sqs_lambda/reflex_aws_cloudtrail_logging_stopped.tf
@@ -2,7 +2,7 @@ module "reflex_aws_cloudtrail_logging_stopped_sqs_lambda" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=region-refactor"
 
   function_name   = "CloudTrailLoggingStopped"
-  source_code_dir = "${path.module}/source"
+  source_code_dir = "${path.module}/../../source"
   handler         = "reflex_aws_cloudtrail_logging_stopped.lambda_handler"
   lambda_runtime  = "python3.7"
   environment_variable_map = {
