837481479d0591468aad9844f1596cb91e677449
rjulian <richard@rjulian.net>
2020-04-30 14:18:12 -0700

    Fix extra underscore in source url

--- terraform/cwe/reflex_aws_cloudtrail_logging_stopped.tf
+++ terraform/cwe/reflex_aws_cloudtrail_logging_stopped.tf
@@ -1,5 +1,5 @@
 module "reflex_aws_cloudtrail_logging_stopped_cwe" {
-  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_?ref=region-refactor"
+  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref=region-refactor"
   rule_name        = "CloudtrailLoggingStopped"
   rule_description = "Detects when a CloudTrail Trail has logging turned off."
 
