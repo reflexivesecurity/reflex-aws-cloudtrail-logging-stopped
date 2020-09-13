d49c65ff542407458f3b7018e843316ac9887d24
rjulian <richard@rjulian.net>
2020-09-13 14:45:15 -0700

    Semi-automated multi account push

--- source/reflex_aws_cloudtrail_logging_stopped.py
+++ source/reflex_aws_cloudtrail_logging_stopped.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class CloudTrailLoggingStopped(AWSRule):
@@ -42,5 +42,9 @@ class CloudTrailLoggingStopped(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = CloudTrailLoggingStopped(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
